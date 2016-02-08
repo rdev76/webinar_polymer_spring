2b9e928171f6554ebb88eeef00e243923d5ebd12
rdev <robert@myradio.nl>
2016-02-08 12:01:43 +0100

    Initial commit Completed demo 2: Polymer

---
+++ src/main/java/com/example/WebinarPolymerSpringApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class WebinarPolymerSpringApplication {
+	//Resource Service
+	@RequestMapping("/resource")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "hello");
+		return model;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebinarPolymerSpringApplication.class, args);
+	}
+}
